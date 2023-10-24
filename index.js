fd98a9bc533330c3cbec1fee63eb731d585292de
Hamin Park <haminjp@naver.com>
2023-10-24 17:26:59 +0900

    feat: add npm package config variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const portNumber = process.env.npm_package_config_port;
+const packageAuthor = process.env.npm_package_config_packageAuthor;
 
 console.log(`Port number: ${portNumber}`);
+console.log(`Package Author: ${packageAuthor}`);
