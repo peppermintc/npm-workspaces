b460c6735b7f64a3fd805792ed979aa27c5b6790
Hamin Park <haminjp@naver.com>
2023-10-25 09:13:50 +0900

    feat: add os field

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const portNumber = process.env.npm_package_config_port;
 const packageAuthor = process.env.npm_package_config_packageAuthor;
+const currentOs = process.platform;
 
 console.log(`Port number: ${portNumber}`);
 console.log(`Package Author: ${packageAuthor}`);
+console.log(`Current OS: ${currentOs}`);
