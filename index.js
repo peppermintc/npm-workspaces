92ac003693e1bf726ccaef07e833a922d4de7ce9
Hamin Park <haminjp@naver.com>
2023-10-25 09:21:04 +0900

    feat: add cpu field

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const portNumber = process.env.npm_package_config_port;
 const packageAuthor = process.env.npm_package_config_packageAuthor;
 const currentOs = process.platform;
+const currentCpuArchitecture = process.arch;
 
 console.log(`Port number: ${portNumber}`);
 console.log(`Package Author: ${packageAuthor}`);
 console.log(`Current OS: ${currentOs}`);
+console.log(`Current CPU Archcitecture: ${currentCpuArchitecture}`);
